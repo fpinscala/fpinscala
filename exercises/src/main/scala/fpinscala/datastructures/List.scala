64c4f39f35c2c638b151584254d1aa281110b28a
Marius Soutier <spybreak@gmx.net>
2013-04-28 11:30:38 +0200

    Added toString method to Cons so it's easier to test

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -2,34 +2,42 @@ package fpinscala.datastructures
 
 sealed trait List[+A] // `List` data type
 case object Nil extends List[Nothing] // data constructor for `List`
-case class Cons[+A](head: A, tail: List[A]) extends List[A]
+case class Cons[+A](head: A, tail: List[A]) extends List[A] {
+  override def toString: String = {
+    def toString0(rest: List[A]): String = rest match {
+      case Cons(a, Nil) => a.toString
+      case Cons(a, as) => a.toString + ", " + toString0(as)
+    }
+    "[" + toString0(this) + "]"
+  }
+}
 
 object List { // `List` companion object
   def sum(ints: List[Int]): Int = ints match { // Pattern matching example
     case Nil => 0
     case Cons(x,xs) => x + sum(xs)
-  } 
-  
+  }
+
   def product(ds: List[Double]): Double = ds match {
     case Nil => 1.0
     case Cons(0.0, _) => 0.0
     case Cons(x,xs) => x * product(xs)
   }
-  
+
   def apply[A](as: A*): List[A] = // Variadic function syntax
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
-  
+
   val example = Cons(1, Cons(2, Cons(3, Nil))) // Creating lists
   val example2 = List(1,2,3)
   val total = sum(example)
 
   val x = List(1,2,3,4,5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42 
+    case Nil => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
     case Cons(h, t) => h + sum(t)
-    case _ => 101 
+    case _ => 101
   }
 
   def append[A](a1: List[A], a2: List[A]): List[A] =
@@ -43,11 +51,11 @@ object List { // `List` companion object
       case Nil => z
       case Cons(x, xs) => f(x, foldRight(xs, z)(f))
     }
-  
-  def sum2(l: List[Int]) = 
+
+  def sum2(l: List[Int]) =
     foldRight(l, 0.0)(_ + _)
-  
-  def product2(l: List[Double]) = 
+
+  def product2(l: List[Double]) =
     foldRight(l, 1.0)(_ * _)
 
 
