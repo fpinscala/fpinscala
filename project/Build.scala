c6ea833121de205356e3f1a38fc01483fa273af7
Seth Tisue <seth@tisue.net>
2013-07-24 14:00:33 -0400

    upgrade to Scala 2.10.2

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object FPInScalaBuild extends Build {
   val opts = Project.defaultSettings ++ Seq(
-    scalaVersion := "2.10.0",
+    scalaVersion := "2.10.2",
     resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
     libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.1.0"
   )
