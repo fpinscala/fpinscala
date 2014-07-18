df3951fabb6187fd3af4e7a537567ae320b1e1f2
abo64 <abo64@gmx.de>
2014-07-18 12:51:23 +0200

    tests for GettingStarted

--- project/Build.scala
+++ project/Build.scala
@@ -3,8 +3,11 @@ import Keys._
 
 object FPInScalaBuild extends Build {
   val opts = Project.defaultSettings ++ Seq(
-    scalaVersion := "2.10.3",
-    resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+    scalaVersion := "2.10.4",
+    resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+    libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.2.0" % "test",
+    libraryDependencies += "junit" % "junit" % "4.10" % "test",
+    libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.11.3" % "test"
   )
 
   lazy val root =
