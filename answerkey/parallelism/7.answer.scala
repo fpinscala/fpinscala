0a2fc3361e1a1df68f9491b468de8529806e17d5
Paul Chiusano <paul.chiusano@gmail.com>
2014-08-06 16:53:12 -0400

    Update 7.answer.scala

--- answerkey/parallelism/7.answer.scala
+++ answerkey/parallelism/7.answer.scala
@@ -1,6 +1,5 @@
 /* 
-See this post by Derek Elkins on the Haskell mailing list, giving the derivation:
-http://www.haskell.org/pipermail/haskell-cafe/2010-January/071636.html
+See https://github.com/quchen/articles/blob/master/second_functor_law.md
 
-Brief note on translating this syntax: In Haskell, `f . g` is function composition, `f compose g` or `(x => f(g(x)))` in Scala. And `fmap` is equivalent to our function `map`, but with the arguments flipped, so `fmap f x == map(x)(f)`.
+Also https://gist.github.com/pchiusano/444de1f222f1ceb09596
 */
