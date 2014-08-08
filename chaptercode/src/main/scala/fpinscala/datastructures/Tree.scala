c79ca8c10cf676dcccd0886318cbe4f27e414106
Paul Chiusano <paul.chiusano@gmail.com>
2014-08-08 14:16:08 -0400

    removed chaptercode

--- chaptercode/src/main/scala/fpinscala/datastructures/Tree.scala
+++
@@ -1,8 +0,0 @@
-package fpinscala.datastructures
-
-sealed trait Tree[+A]
-case class Leaf[A](value: A) extends Tree[A]
-case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
-
-object Tree {
-}
