6051ab4ce9b65798d9071acab590eaa3355d901d
Paul Chiusano <paul.chiusano@gmail.com>
2014-08-08 10:46:25 -0400

    Create 2.answer.scala

---
+++ answerkey/iomonad/2.answer.scala
@@ -0,0 +1,10 @@
+  @annotation.tailrec
+  def runTrampoline[A](a: Free[Function0,A]): A = (a) match {
+    case Return(a) => a
+    case Suspend(r) => r()
+    case FlatMap(x,f) => x match {
+      case Return(a) => runTrampoline { f(a) }
+      case Suspend(r) => runTrampoline { f(r()) }
+      case FlatMap(a0,g) => runTrampoline { a0 flatMap { a0 => g(a0) flatMap f } }
+    }
+  }
