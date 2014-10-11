2b7187d8dd4cc755c267172c0bb6533335c5909f
Rúnar <runarorama@gmail.com>
2014-10-10 23:56:22 -0400

    Update 03.answer.scala
    Should be `&&` instead of `||`

--- answerkey/testing/03.answer.scala
+++ answerkey/testing/03.answer.scala
@@ -1,4 +1,4 @@
 /* We can refer to the enclosing `Prop` instance with `Prop.this` */
 def &&(p: Prop): Prop = new Prop {
-  def check = Prop.this.check || p.check
+  def check = Prop.this.check && p.check
 }
