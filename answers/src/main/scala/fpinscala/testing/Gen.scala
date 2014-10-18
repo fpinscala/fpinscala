00d55be155948d17a8c108adae9ae5e2fef7d031
Takahiro Noda <takahiro.noda+github@gmail.com>
2014-10-18 17:08:54 +0900

    Fix a bug in the answer of Exercise 8.8
    The flatMap function should take a function that returns Gen[A], not A.

--- answers/src/main/scala/fpinscala/testing/Gen.scala
+++ answers/src/main/scala/fpinscala/testing/Gen.scala
@@ -226,8 +226,7 @@ object Gen {
   def weighted[A](g1: (Gen[A],Double), g2: (Gen[A],Double)): Gen[A] = {
     /* The probability we should pull from `g1`. */
     val g1Threshold = g1._2.abs / (g1._2.abs + g2._2.abs)
-
-    Gen(State(RNG.double).flatMap(d => if (d < g1Threshold) g1._1.sample else g2._1.sample))
+    Gen(State(RNG.double).flatMap(d => (if (d < g1Threshold) g1 else g2)._1))
   }
 
   def listOf[A](g: Gen[A]): SGen[List[A]] =
