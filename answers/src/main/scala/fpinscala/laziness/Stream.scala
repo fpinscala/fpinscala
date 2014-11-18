5082551f4b5d97ee48d1ed0cfcc6065b038375ea
Mads André Lundeland <lundeland@gmail.com>
2014-11-18 15:39:06 +0100

    Prevent unnecessary evaluations
    Previous implementation of drop did not handle 
        
        Stream.cons(0, ???).take(1)

--- answers/src/main/scala/fpinscala/laziness/Stream.scala
+++ answers/src/main/scala/fpinscala/laziness/Stream.scala
@@ -43,13 +43,19 @@ trait Stream[+A] {
     go(this)
   }
 
-  
-
+  /*
+    Create a new Stream from taking the n first elements from this.
+  */
   def take(n: Int): Stream[A] = this match {
-    case Cons(h, t) if n > 0 => cons[A](h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
     case _ => empty
   }
   
+  /* 
+    Unlike `take`, `drop` is not incremental. That is, it doesn't generate the
+    answer lazily. It must traverse the first `n` elements of the stream eagerly.
+  */
   @annotation.tailrec
   def drop(n: Int): Stream[A] = this match {
     case Cons(h, t) if n > 0 => t().drop(n - 1)
