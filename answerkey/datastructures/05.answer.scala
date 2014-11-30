5d5773578d1f859a863e09a4c41ed2ff99d6e8c7
Rúnar <runarorama@gmail.com>
2014-11-30 10:59:50 -0500

    Update 05.answer.scala

--- answerkey/datastructures/05.answer.scala
+++ answerkey/datastructures/05.answer.scala
@@ -1,5 +1,8 @@
 /* 
-Somewhat overkill, but to illustrate the feature we're using a _pattern guard_, to only match a `Cons` whose head satisfies our predicate, `f`. The syntax is to add `if <cond>` after the pattern, before the `=>`, where `<cond>` can use any of the variables introduced by the pattern.
+Somewhat overkill, but to illustrate the feature we're using a _pattern guard_,
+to only match a `Cons` whose head satisfies our predicate, `f`. 
+The syntax is to add `if <cond>` after the pattern, before the `=>`, 
+where `<cond>` can use any of the variables introduced by the pattern.
 */
 def dropWhile[A](l: List[A], f: A => Boolean): List[A] = 
   l match {
