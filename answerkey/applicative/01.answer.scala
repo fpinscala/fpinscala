0147462160db6800d3b00ce74ee697287c27379f
Marek Kadek <kadek.marek@gmail.com>
2014-12-27 06:58:27 +0100

    Chapter12, answerkey product signature fix

--- answerkey/applicative/01.answer.scala
+++ answerkey/applicative/01.answer.scala
@@ -4,5 +4,5 @@ def sequence[A](fas: List[F[A]]): F[List[A]] =
 def replicateM[A](n: Int, fa: F[A]): F[List[A]] =
   sequence(List.fill(n)(fa))
 
-def product[A,B](fa: F[A], fb: F[A]): F[(A,B)] =
+def product[A,B](fa: F[A], fb: F[B]): F[(A,B)] =
   map2(fa, fb)((_,_))
