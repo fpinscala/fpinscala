b2128a1b0f5704d9b398ac90192e40839d04b5ee
Jérémie Astori <jeremie@astori.fr>
2014-12-29 23:54:20 -0500

    Add sequenceViaTraverse to Option answers (chapter 4, exercise 5)

--- answers/src/main/scala/fpinscala/errorhandling/Option.scala
+++ answers/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -101,4 +101,7 @@ object Option {
 
   def traverse_1[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
     a.foldRight[Option[List[B]]](Some(Nil))((h,t) => map2(f(h),t)(_ :: _))
+
+  def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] =
+    traverse(a)(x => x)
 }
