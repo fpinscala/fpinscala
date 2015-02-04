41abc3140fae48e09398ad02c8a7221a7b16e94d
Michael Ummels <michael@ummels.de>
2015-02-04 21:35:50 +0100

    Fix answer for Exercise 12.12
    Also replace code in answers project with simpler solution

--- answers/src/main/scala/fpinscala/applicative/Applicative.scala
+++ answers/src/main/scala/fpinscala/applicative/Applicative.scala
@@ -66,9 +66,8 @@ trait Applicative[F[_]] extends Functor[F] {
   }
 
   def sequenceMap[K,V](ofa: Map[K,F[V]]): F[Map[K,V]] =
-    ofa.foldLeft(unit(Map[K,V]())) {
-      case (acc, (k, fv)) => apply(map(acc)(m =>
-        (n: Map[K,V]) => m ++ n))(map(fv)((v: V) => Map(k -> v)))
+    (ofa foldLeft unit(Map.empty[K,V])) { case (acc, (k, fv)) =>
+      map2(acc, fv)((m, v) => m + (k -> v))
     }
 
 }
