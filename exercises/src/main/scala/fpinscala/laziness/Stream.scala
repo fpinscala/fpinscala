a42d0888b0c53572a566d3d0616f57190a01c7c5
Guillaume Poirier <guillaume.poirier@mediative.com>
2015-03-04 12:57:18 -0500

    Implementation of map, take, takeWhile, zipWith and zipAll using unfold.

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -22,9 +22,14 @@ trait Stream[+A] {
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
   def take(n: Int): Stream[A] = this match {
-    case _ if n <= 0 => Empty
-    case Empty => Empty
-    case Cons(head, tail) => Stream.cons(head(), tail().take(n - 1))
+    case Cons(head, tail) if n == 1 => Stream.cons(head(), Empty)
+    case Cons(head, tail) if n > 1 => Stream.cons(head(), tail().take(n - 1))
+    case _ => Empty
+  }
+  def takeUnfold(n: Int): Stream[A] = unfold(this -> n) { 
+    case (_, 0) | (Empty, _) => None
+    case (Cons(head, _), 1) => Some((head(), Empty -> 0))
+    case (Cons(head, tail), count) => Some((head(), tail() -> (count - 1)))
   }
 
   @annotation.tailrec
@@ -39,6 +44,11 @@ trait Stream[+A] {
     else Empty
   }
 
+  def takeWhileUnfold(p: A => Boolean): Stream[A] = unfold(this) {
+    case Cons(head, tail) if p(head()) => Some(head() -> tail())
+    case _ => None
+  }
+
   def forAll(p: A => Boolean): Boolean = !exists(!p(_))
 
   def headOption: Option[A] = foldRight(Option.empty[A]) { (a, b) => Some(a) }
@@ -48,6 +58,11 @@ trait Stream[+A] {
 
   def map[B](f: A => B): Stream[B] = foldRight(Stream.empty[B]) { (a, b) =>
     Stream.cons(f(a), b)
+  }
+
+  def mapUnfold[B](f: A => B): Stream[B] = unfold(this) {
+    case Cons(head, tail) => Some(f(head()) -> tail())
+    case _ => None
   }
 
   def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight(Stream.empty[B]) { (a, b) =>
@@ -68,6 +83,18 @@ trait Stream[+A] {
   def toList: List[A] = this match {
     case Empty => Nil
     case Cons(head, tail) => head() :: tail().toList
+  }
+
+  def zipWith[B, C](that: Stream[B])(f: (A, B) => C): Stream[C] = unfold(this -> that) {
+    case (Cons(h1, t1), Cons(h2, t2)) => Some((f(h1(), h2()), t1() -> t2()))
+    case _ => None
+  }
+
+  def zipAll[B](that: Stream[B]): Stream[(Option[A], Option[B])] = unfold(this -> that) {
+    case (Cons(h1, t1), Cons(h2, t2)) => Some(((Some(h1()), Some(h2())), t1() -> t2()))
+    case (Cons(h1, t1), Empty) => Some(((Some(h1()), None), t1() -> Empty))
+    case (Empty, Cons(h2, t2)) => Some(((None, Some(h2())), Empty -> t2()))
+    case _ => None
   }
 }
 
