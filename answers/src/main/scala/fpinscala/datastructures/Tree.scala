5bf1138f3aa71ff91babaa99613313fb9ac48b27
Alosh Bennett <aloshbennett@gmail.com>
2015-03-18 20:49:34 +0530

    Split lengthy comments into multiple lines for ease of reading. The comments are now under 120 characters.

--- answers/src/main/scala/fpinscala/datastructures/Tree.scala
+++ answers/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -38,7 +38,9 @@ object Tree {
   }
 
   /* 
-  Like `foldRight` for lists, `fold` receives a "handler" for each of the data constructors of the type, and recursively accumulates some value using these handlers. As with `foldRight`, `fold(t)(Leaf(_))(Branch(_,_)) == t`, and we can use this function to implement just about any recursive function that would otherwise be defined by pattern matching.
+  Like `foldRight` for lists, `fold` receives a "handler" for each of the data constructors of the type, and recursively
+  accumulates some value using these handlers. As with `foldRight`, `fold(t)(Leaf(_))(Branch(_,_)) == t`, and we can use
+  this function to implement just about any recursive function that would otherwise be defined by pattern matching.
   */
   def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = t match {
     case Leaf(a) => f(a)
@@ -63,7 +65,12 @@ object Tree {
        fold(t)(a => Leaf(f(a)))(Branch(_,_))
                                       ^  
   
-  This error is an unfortunate consequence of Scala using subtyping to encode algebraic data types. Without the annotation, the result type of the fold gets inferred as `Leaf[B]` and it is then expected that the second argument to `fold` will return `Leaf[B]`, which it doesn't (it returns `Branch[B]`). Really, we'd prefer Scala to infer `Tree[B]` as the result type in both cases. When working with algebraic data types in Scala, it's somewhat common to define helper functions that simply call the corresponding data constructors but give the less specific result type:  
+  This error is an unfortunate consequence of Scala using subtyping to encode algebraic data types. Without the
+  annotation, the result type of the fold gets inferred as `Leaf[B]` and it is then expected that the second argument
+  to `fold` will return `Leaf[B]`, which it doesn't (it returns `Branch[B]`). Really, we'd prefer Scala to
+  infer `Tree[B]` as the result type in both cases. When working with algebraic data types in Scala, it's somewhat
+  common to define helper functions that simply call the corresponding data constructors but give the less specific
+  result type:
     
     def leaf[A](a: A): Tree[A] = Leaf(a)
     def branch[A](l: Tree[A], r: Tree[A]): Tree[A] = Branch(l, r)
