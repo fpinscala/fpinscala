d9476b3323e843d234454ac411a61d353809deee
Alosh Bennett <aloshbennett@gmail.com>
2015-03-20 00:01:21 +0530

    Split lengthy comments from chapter errorhandling into multiple lines

--- answers/src/main/scala/fpinscala/errorhandling/Either.scala
+++ answers/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -1,7 +1,8 @@
 package fpinscala.errorhandling
 
 
-import scala.{Option => _, Either => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
+//hide std library `Option` and `Either`, since we are writing our own in this chapter
+import scala.{Option => _, Either => _, _}
 
 sealed trait Either[+E,+A] {
  def map[B](f: A => B): Either[E, B] = 
@@ -54,14 +55,19 @@ object Either {
     traverse(es)(x => x)
 
   /*
-  There are a number of variations on `Option` and `Either`. If we want to accumulate multiple errors, a simple approach is a new data type that lets us keep a list of errors in the data constructor that represents failures:
+  There are a number of variations on `Option` and `Either`. If we want to accumulate multiple errors, a simple
+  approach is a new data type that lets us keep a list of errors in the data constructor that represents failures:
   
   trait Partial[+A,+B]
   case class Errors[+A](get: Seq[A]) extends Partial[A,Nothing]
   case class Success[+B](get: B) extends Partial[Nothing,B]
   
-  There is a type very similar to this called `Validation` in the Scalaz library. You can implement `map`, `map2`, `sequence`, and so on for this type in such a way that errors are accumulated when possible (`flatMap` is unable to accumulate errors--can you see why?). This idea can even be generalized further--we don't need to accumulate failing values into a list; we can accumulate values using any user-supplied binary function. 
+  There is a type very similar to this called `Validation` in the Scalaz library. You can implement `map`, `map2`,
+  `sequence`, and so on for this type in such a way that errors are accumulated when possible (`flatMap` is unable to
+  accumulate errors--can you see why?). This idea can even be generalized further--we don't need to accumulate failing
+  values into a list; we can accumulate values using any user-supplied binary function.
   
-  It's also possible to use `Either[List[E],_]` directly to accumulate errors, using different implementations of helper functions like `map2` and `sequence`.
+  It's also possible to use `Either[List[E],_]` directly to accumulate errors, using different implementations of
+  helper functions like `map2` and `sequence`.
   */
 }
