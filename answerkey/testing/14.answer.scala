043cbe29f80e9962e6e6abf9a487e584bedf5b47
Korneliusz Caputa <helluinster@gmail.com>
2015-05-31 18:48:46 +0200

    Fix: verify ordering on the sorted list, not the original.

--- answerkey/testing/14.answer.scala
+++ answerkey/testing/14.answer.scala
@@ -2,7 +2,7 @@ val sortedProp = forAll(listOf(smallInt)) { ns =>
   val nss = ns.sorted
   // We specify that every sorted list is either empty, has one element,
   // or has no two consecutive elements `(a,b)` such that `a` is greater than `b`.
-  (ns.isEmpty || nss.tail.isEmpty || !ns.zip(ns.tail).exists {
+  (nss.isEmpty || nss.tail.isEmpty || !nss.zip(nss.tail).exists {
     case (a,b) => a > b
   })
     // Also, the sorted list should have all the elements of the input list,
