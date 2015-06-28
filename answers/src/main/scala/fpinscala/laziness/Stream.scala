ac7162b1f6a53bf622625adadb8cda3a320fc981
Lars Westergren <lars.westergren@gmail.com>
2015-06-28 13:41:57 +0200

    Laziness chapter answers, a simpler zipAll that doesn't rely on slim arrow operator in zipWithAll.

--- answers/src/main/scala/fpinscala/laziness/Stream.scala
+++ answers/src/main/scala/fpinscala/laziness/Stream.scala
@@ -140,8 +140,16 @@ trait Stream[+A] {
   def zip[B](s2: Stream[B]): Stream[(A,B)] =
     zipWith(s2)((_,_))
 
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A],Option[B])] = {
+    unfold((this,s2)) {
+      case((Empty, Empty)) => None
+      case ((Empty, Cons(h,t))) => Some(((None, Some(h())),(Empty, t())))
+      case ((Cons(h,t), Empty)) => Some(((Some(h()), None),(t(), Empty)))
+      case ((Cons(h1,t1), Cons(h2, t2))) =>Some(( (Some(h1()), Some(h2())), (t1(), t2())))
+    }
+  }
 
-  def zipAll[B](s2: Stream[B]): Stream[(Option[A],Option[B])] =
+  def zipAllViaZipWithAll[B](s2: Stream[B]): Stream[(Option[A],Option[B])] =
     zipWithAll(s2)((_,_))
 
   def zipWithAll[B, C](s2: Stream[B])(f: (Option[A], Option[B]) => C): Stream[C] =
