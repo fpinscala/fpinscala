fb8acf7c098fa9a585141ddbbbfd4046d69e6acf
pegomes <pegomes@yahoo.com>
2015-11-21 14:50:16 -0800

    Fix for issue #364. Implemented simplification proposed by gothug.

--- answers/src/main/scala/fpinscala/laziness/Stream.scala
+++ answers/src/main/scala/fpinscala/laziness/Stream.scala
@@ -118,8 +118,7 @@ trait Stream[+A] {
 
   def takeViaUnfold(n: Int): Stream[A] =
     unfold((this,n)) {
-      case (Cons(h,t), 1) => Some((h(), (empty, 0)))
-      case (Cons(h,t), n) if n > 1 => Some((h(), (t(), n-1)))
+      case (Cons(h,t), n) if n > 0 => Some((h(), (t(), n-1)))
       case _ => None
     }
 
