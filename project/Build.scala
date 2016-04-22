4bb658329dcd1005e2c1ee033344d1a05ee94fcd
Rémi Kowalski <remi.florian.kowalski@gmail.com>
2016-04-22 16:21:19 +0200

    fix build for usage with ensime-sbt

--- project/Build.scala
+++ project/Build.scala
@@ -2,29 +2,27 @@ import sbt._
 import Keys._
 
 object FPInScalaBuild extends Build {
-  val opts = Project.defaultSettings ++ Seq(
+  val opts = Seq(
     scalaVersion := "2.11.7",
     resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
   )
 
   lazy val root =
     Project(id = "fpinscala",
-            base = file("."),
-            settings = opts ++ Seq(
+            base = file(".")).
+            settings(opts ++ Seq(
               onLoadMessage ~= (_ + nio2check())
             )) aggregate (chapterCode, exercises, answers)
   lazy val chapterCode =
     Project(id = "chapter-code",
-            base = file("chaptercode"),
-            settings = opts)
+            base = file("chaptercode")).settings( opts)
   lazy val exercises =
     Project(id = "exercises",
-            base = file("exercises"),
-            settings = opts)
+            base = file("exercises")).settings(opts)
   lazy val answers =
     Project(id = "answers",
-            base = file("answers"),
-            settings = opts)
+            base = file("answers")).
+            settings(opts)
 
   def nio2check(): String = {
     val cls = "java.nio.channels.AsynchronousFileChannel"
