ffd02aae9b85f60c2a7d0d91efc6c95a8e41a570
Akinwale Alexander <waleander@gmail.com>
2016-05-11 04:10:32 +0100

    Update 01.answer.scala
    subtract 2 from the initial value of n to satisfy the invariant

--- answerkey/gettingstarted/01.answer.scala
+++ answerkey/gettingstarted/01.answer.scala
@@ -3,7 +3,7 @@ def fib(n: Int): Int = {
   def loop(n: Int, prev: Int, cur: Int): Int =
     if (n == 0) prev 
     else loop(n - 1, cur, prev + cur)
-  loop(n, 0, 1)
+  loop(n - 2, 0, 1)
 }
 
 // 0 and 1 are the first two numbers in the sequence, so we start the accumulators with those.
