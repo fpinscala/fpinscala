2e094527aa4e0905a91259071e912df2a6ce3759
hanze.cui <hanze.cui@qunar.com>
2016-10-10 13:38:17 +0800

    add

--- answers/src/main/scala/fpinscala/laziness/Stream.scala
+++ answers/src/main/scala/fpinscala/laziness/Stream.scala
@@ -91,7 +91,7 @@ trait Stream[+A] {
   def takeWhile_1(f: A => Boolean): Stream[A] =
     foldRight(empty[A])((h,t) =>
       if (f(h)) cons(h,t)
-      else      empty)
+      else      t)
 
   def headOption: Option[A] =
     foldRight(None: Option[A])((h,_) => Some(h))
