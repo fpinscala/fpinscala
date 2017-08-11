f31e14f87a6ad32712fabfcd831aec54a3a1a6cf
Ascander Dost <ascanderdost@gmail.com>
2017-08-11 11:25:28 -0700

    finishes Ch.4 exercises

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -1,92 +1,79 @@
 package fpinscala.errorhandling
 
-// hide std library `Option` and `Either`, since we are writing our own in this chapter
-import scala.{Either => _, Left => _, Option => _, Right => _}
+import scala.{Either => _, Left => _, Option => _, Right => _} // hide std library versions
 
 
-sealed trait Either[+E,+A] {
+sealed trait Either[+E, +A] {
 
- /**
-   * Exercise 4.6 - Implement versions of map, flatMap, orElse, and map2 on Either that operate on the Right value.
-   */
+  /**
+    * Exercise 4.6
+    *
+    * Implement versions of map, flatMap, orElse, and map2 on Either that operate on the Right
+    * value.
+    */
 
- def map[B](f: A => B): Either[E, B] = this match {
-   case Left(e)  => Left(e)
-   case Right(a) => Right(f(a))
- }
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(e)  => Left(e)
+    case Right(a) => Right(f(a))
+  }
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
-   case Left(e)  => Left(e)
-   case Right(a) => f(a)
- }
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e)  => Left(e)
+    case Right(a) => f(a)
+  }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
-   case Left(_)  => b
-   case Right(a) => Right(a)
- }
+  def orElse[EE >: E, B >: A](b: Either[EE, B]): Either[EE, B] = this match {
+    case Left(_)  => b
+    case Right(a) => Right(a)
+  }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
-   flatMap (a => b map (bb => f(a, bb)))
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    flatMap(a => b map (bb => f(a, bb)))
 
- /**
-   * Alternative implementation that uses a for-comprehension.
-   */
- def map3[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
-   for { aa <- this ; bb <- b } yield f(aa, bb)
-
+  /** Alternative implementation that uses a for-comprehension. */
+  def map2ForComprehension[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {aa <- this; bb <- b} yield f(aa, bb)
 }
 
-case class Left[+E](get: E) extends Either[E,Nothing]
-case class Right[+A](get: A) extends Either[Nothing,A]
+case class Left[+E](get: E) extends Either[E, Nothing]
+case class Right[+A](get: A) extends Either[Nothing, A]
 
 object Either {
 
   /**
-    * Exercise 4.7 - Implement sequence and traverse for Either. These should return the first error that’s encountered,
-    * if there is one.
+    * Exercise 4.7
+    *
+    * Implement sequence and traverse for Either. These should return the first error that’s
+    * encountered, if there is one.
     */
 
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+  def traverse[E, A, B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
     es.foldRight(Right(Nil): Either[E, List[B]])((a, acc) => f(a).map2(acc)(_ :: _))
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] =
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
     traverse(es)(identity)
 
-  def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
-    if (xs.isEmpty) 
-      Left("mean of empty list!")
-    else 
-      Right(xs.sum / xs.length)
-
-  def safeDiv(x: Int, y: Int): Either[Exception, Int] = 
-    try Right(x / y)
-    catch { case e: Exception => Left(e) }
-
-  def Try[A](a: => A): Either[Exception, A] =
-    try Right(a)
-    catch { case e: Exception => Left(e) }
-
   /**
-    * Exercise 4.8 - In this implementation, map2 is only able to report one error, even if both the name and the age
-    * are invalid. What would you need to change in order to report both errors? Would you change map2 or the signature
-    * of mkPerson? Or could you create a new data type that captures this requirement better than Either does, with some
-    * additional structure? How would orElse, traverse, and sequence behave differently for that data type?
+    * Exercise 4.8
+    *
+    * In this implementation, map2 is only able to report one error, even if both the name and the
+    * age are invalid. What would you need to change in order to report both errors? Would you
+    * change map2 or the signature of mkPerson? Or could you create a new data type that captures
+    * this requirement better than Either does, with some additional structure? How would orElse,
+    * traverse, and sequence behave differently for that data type?
+    *
+    * ==Answer==
+    * If we want to accumulate multiple errors, a simple approach would be a new data type that
+    * allows us to keep a list of errors in the constructor that represents failures:
+    *
+    * {{{
+    *   trait Partial[+A, +B]
+    *   case class Errors[+A](get: Seq[A]) extends Partial[A, Nothing]
+    *   case class Success[+B](get: B) extends Partial[Nothing, B]
+    * }}}
+    *
+    * We would need to implement map, map2, sequence, and so on for this type in such a way as to
+    * accumulate errors when possible. Interestingly, flatMap cannot be written in such a way,
+    * because it short-circuits on failure cases, making it impossible to collect multiple failures.
     */
-
-  /*
-   * There are a number of variations on `Option` and `Either`. If we want to accumulate multiple errors, a simple
-   * approach is a new data type that lets us keep a list of errors in the data constructor that represents failures:
-   *
-   * trait Partial[+A,+B]
-   * case class Errors[+A](get: Seq[A]) extends Partial[A,Nothing]
-   * case class Success[+B](get: B) extends Partial[Nothing,B]
-   *
-   * There is a type very similar to this called `Validation` in the Scalaz library. You can implement `map`, `map2`,
-   * `sequence`, and so on for this type in such a way that errors are accumulated when possible (`flatMap` is unable to
-   * accumulate errors--can you see why?). This idea can even be generalized further--we don't need to accumulate
-   * failing values into a list; we can accumulate values using any user-supplied binary function.
-   *
-   * It's also possible to use `Either[List[E],_]` directly to accumulate errors, using different implementations of
-   * helper functions like `map2` and `sequence`.
-   */
 }
--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,20 +1,22 @@
 package fpinscala.errorhandling
 
-// hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
-import scala.{Either => _, Option => _, Some => _}
+import scala.{Either => _, Option => _, Some => _} // hide std library versions
 
 
 sealed trait Option[+A] {
 
   /**
-    * Exercise 4.1 - Implement all of the preceding functions on Option. As you implement each function, try to think
-    * about what it means and in what situations you’d use it.  We’ll explore when to use each of these functions next.
-    * Here are a few hints for solving this exercise:
+    * Exercise 4.1
     *
-    *   - It’s fine to use pattern matching, though you should be able to implement all the functions besides map and
-    *     getOrElse without resorting to pattern matching.
+    * Implement all of the preceding functions on Option. As you implement each function, try to
+    * think about what it means and in what situations you’d use it. We’ll explore when to use each
+    * of these functions next. Here are a few hints for solving this exercise:
+    *
+    *   - It’s fine to use pattern matching, though you should be able to implement all the
+    * functions besides map and getOrElse without resorting to pattern matching.
     *   - For map and flatMap, the type signature should be enough to determine the implementation.
-    *   - getOrElse returns the result inside the Option, or if the Option is None, returns the default value.
+    *   - getOrElse returns the result inside the Some case of the Option, or if the Option is None,
+    * returns the given default value.
     *   - orElse returns the first Option if it’s defined; otherwise, it returns the second Option.
     */
 
@@ -23,16 +25,16 @@ sealed trait Option[+A] {
     case Some(a) => Some(f(a))
   }
 
-  def getOrElse[B>:A](default: => B): B = this match {
+  def getOrElse[B >: A](default: => B): B = this match {
     case None    => default
     case Some(a) => a
   }
 
   def flatMap[B](f: A => Option[B]): Option[B] =
-    map (f) getOrElse None
+    map(f) getOrElse None
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] =
-    map (Some(_)) getOrElse ob
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
+    map(Some(_)) getOrElse ob
 
   def filter(f: A => Boolean): Option[A] =
     flatMap(a => if (f(a)) Some(a) else None)
@@ -43,65 +45,58 @@ case object None extends Option[Nothing]
 
 object Option {
 
-  //  def failingFn(i: Int): Int = {
-//    // `val y: Int = ...` declares `y` as having type `Int`, and sets it equal to the right hand side of the `=`.
-//    val y: Int = throw new Exception("fail!")
-//    try {
-//      val x = 42 + 5
-//      x + y
-//    }
-//
-//    /*
-//     * A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that
-//     * matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
-//     */
-//
-//    catch { case e: Exception => 43 }
-//  }
-
-  def failingFn2(i: Int): Int = {
-    try {
-      val x = 42 + 5
-      // A thrown Exception can be given any type; here we're annotating it with the type `Int`
-      x + ((throw new Exception("fail!")): Int)
-    }
-    catch { case e: Exception => 43 }
-  }
-
+  /** Returns the mean of a sequence of doubles. */
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None else Some(xs.sum / xs.length)
 
   /**
-    * Exercise 4.2 - Implement the variance function in terms of flatMap.
+    * Exercise 4.2
+    *
+    * Implement the variance function in terms of flatMap. If the mean of a sequence is m, the
+    * variance is the mean of math.pow(x - m, 2) for each element x in the sequence. See the
+    * definition of variance on Wikipedia (http://mng.bz/0Qsr).
     */
   def variance(xs: Seq[Double]): Option[Double] =
     mean(xs) flatMap (m => mean(xs map (x => math.pow(x - m, 2))))
 
   /**
-    * Exercise 4.3 - Write a generic function map2 that combines two Option values using a binary function. If either
-    * Option value is None, then the return value is too.
+    * Exercise 4.3
+    *
+    * Write a generic function map2 that combines two Option values using a binary function. If
+    * either Option value is None, then the return value is too.
     */
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
     a flatMap (aa => b map (bb => f(aa, bb)))
 
+  /** Version of map2 implemented using a for-comprehension for illustration. */
+  def map2ForComprehension[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    for {
+      aa <- a
+      bb <- b
+    } yield f(aa, bb)
+
   /**
-    * Exercise 4.4 - Write a function sequence that combines a list of Options into one Option containing a list of all
-    * the Some values in the original list. If the original list contains None even once, the result of the function
-    * should be None; otherwise the result should be Some with a list of all the values.
+    * Exercise 4.4
     *
-    * Note that this implementation is not stack-safe. A stack-safe version can be provided using `reverse` and
-    * `foldLeft` without changing anything else.
+    * Write a function sequence that combines a list of Options into one Option containing a list of
+    * all the Some values in the original list. If the original list contains None even once, the
+    * result of the function should be None; otherwise the result should be Some with a list of all
+    * the values.
     */
   def sequence[A](as: List[Option[A]]): Option[List[A]] =
     as.foldRight[Option[List[A]]](Some(Nil))((a, acc) => map2(a, acc)(_ :: _))
 
   /**
-    * Exercise 4.5 - Implement this function. It’s straightforward to do using map and sequence, but try for a more
-    * efficient implementation that only looks at the list once. In fact, implement sequence in terms of traverse.
+    * Exercise 4.5
+    *
+    * Implement this function. It’s straightforward to do using map and sequence, but try for a more
+    * efficient implementation that only looks at the list once. In fact, implement sequence in
+    * terms of traverse.
     */
   def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] =
     as.foldRight(Some(Nil): Option[List[B]])((a, acc) => map2(f(a), acc)(_ :: _))
 
-  def sequence2[A](as: List[Option[A]]): Option[List[A]] =
+  /** Version of sequence in terms of traverse for illustration. */
+  def sequenceViaTraverse[A](as: List[Option[A]]): Option[List[A]] =
     traverse(as)(identity)
 }
