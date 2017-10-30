e7a8b3ad7a12c1c53235512dfe132123aa280b3e
Ascander Dost <adost@salesforce.com>
2017-10-29 18:43:22 -0700

    finished Ch.6

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -1,11 +1,12 @@
 package fpinscala.state
 
+import fpinscala.state.State.{get, modify, sequence}
+
 
 trait RNG {
   // Should generate a random `Int`. We'll later define other functions in terms of `nextInt`.
   def nextInt: (Int, RNG)
 }
-
 
 object RNG {
 
@@ -135,8 +136,8 @@ object RNG {
     *
     * Hard: If you can combine two `RNG` transitions, you should be able to combine a whole list of
     * them. Implement `sequence` for combining a `List` of transitions into a single transition. Use
-    * it to reimplement the `ints` function you wrote before. For the latter, you can use the standard
-    * library function `List.fill(n)(x)` to make a list with `x` repeated `n` times.
+    * it to reimplement the `ints` function you wrote before. For the latter, you can use the
+    * standard library function `List.fill(n)(x)` to make a list with `x` repeated `n` times.
     */
   def sequence[A](rs: List[Rand[A]]): Rand[List[A]] =
     rs.foldRight(unit(Nil: List[A]))((r, acc) => map2(r, acc)(_ :: _))
@@ -145,160 +146,217 @@ object RNG {
   def intsViaSequence(count: Int): Rand[List[Int]] =
     sequence(List.fill(count)(int))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   /**
-    * Exercise 6.8 - Implement `flatMap`, and then use it to implement `nonNegativeLessThan`.
+    * Exercise 6.8
     *
-    * Note that we perform the state transition in our function `f` and apply the function `g` to the returned result.
-    * Applying `g(a)` to the new state is what is responsible for "flattening" the return value; we manage the passing
-    * of states inside the function.
+    * Implement `flatMap`, and then use it to implement `nonNegativeLessThan`.
     */
+  def flatMap[A, B](ra: Rand[A])(f: A => Rand[B]): Rand[B] =
+    rng => { val (a, rng1) = ra(rng) ; f(a)(rng1) }
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
-    val (a, rng1) = f(rng)
-
-    g(a)(rng1)
-  }
+  /** Note main logic is taken from the book implementation. */
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt) {
+      i => val mod = i % n ; if (i + (n - 1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    }
 
   /**
-    * Remember that `flatMap` evaluates its `g` function with respect to the new state obtained by first evaluating its
-    * `f` function - in the case of `nonNegativeLessThan` we are able to recursively call the same function while
-    * automatically passing the newest states.
+    * Exercise 6.9
+    *
+    * Reimplement `map` and `map2` in terms of `flatMap`. The fact that this is possible is what
+    * we’re referring to when we say that `flatMap` is more powerful than `map` and `map2`.
+    *
+    * ==Derivation==
+    *
+    * mapViaFlatMap(int)(_ + 1)
+    *
+    * becomes:
+    * flatMap(int)(i => unit(i + 1))
+    *
+    * becomes:
+    * r => { val (i, r2) = int(r) ; unit(i + 1)(r1) }
     */
-  def nonNegativeLessThan(n: Int): Rand[Int] = flatMap(nonNegativeInt) {
-    i => val mod = i % n ; if (i + (n - 1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
-  }
+  def mapViaFlatMap[A, B](ra: Rand[A])(f: A => B): Rand[B] =
+    flatMap(ra)(a => unit(f(a)))
 
   /**
-    * Exercise 6.9 - Re-implement `map` and `map2` in terms of `flatMap`. The fact that this is possible is what we’re
-    * referring to when we say that `flatMap` is more powerful than `map` and `map2`.
+    * ==Derivation==
+    *
+    * map2ViaFlatMap(int, double)((_, _))
+    *
+    * becomes:
+    * flatMap(int)(i => mapViaFlatMap(double)(d => (i, d)))
+    *
+    * becomes:
+    * flatMap(int)(i => flatMap(double)(d => unit((i, d))))
+    *
+    * becomes:
+    * rng => { val (i, rng2) = int(rng); flatMap(double)(d => unit((i, d)))(rng2) }
+    *
+    * becomes:
+    * val inner = rng => { val (d, rng2) = double(rng); unit((i, d))(rng2) }
+    * rng => { val (i, rng2) = int(rng); inner(rng2) }
     */
-
-  def mapViaFlatMap[A, B](s: Rand[A])(f: A => B): Rand[B] =
-    flatMap(s)(a => unit(f(a)))
-
   def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
     flatMap(ra)(a => mapViaFlatMap(rb)(b => f(a, b)))
 }
 
 /**
-  * Exercise 6.10 - Generalize the functions `unit`, `map`, `map2`, `flatMap`, and `sequence`. Add them as methods on
-  * the `State` case class where possible. Otherwise you should put them in a `State` companion object.
+  * Exercise 6.10
+  *
+  * Generalize the functions `unit`, `map`, `map2`, `flatMap`, and `sequence`. Add them as methods
+  * on the `State` case class where possible. Otherwise you should put them in a `State` companion
+  * object.
   */
+
 case class State[S, +A](run: S => (A, S)) {
+
   def map[B](f: A => B): State[S, B] =
     flatMap(a => State.unit(f(a)))
 
-  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] = for {
-      a <- this
-      b <- sb
-    } yield f(a, b)
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap(a => sb.map(b => f(a, b)))
 
   def flatMap[B](f: A => State[S, B]): State[S, B] =
     State(s => { val (a, s2) = run(s); f(a).run(s2) })
 }
 
-sealed trait Input
-case object Coin extends Input
-case object Turn extends Input
-
-case class Machine(locked: Boolean, candies: Int, coins: Int)
-
 object State {
 
-  def unit[S, A](a: A): State[S, A] =
-    State(s => (a, s))
+  def unit[S, A](a: A): State[S, A] = State(s => (a, s))
 
-  def sequence[S, A](ss: List[State[S, A]]): State[S, List[A]] =
-    ss.foldRight(unit(Nil): State[S, List[A]])((s, acc) => { for { h <- s ; t <- acc } yield h :: t })
+  def sequence[S, A](as: List[State[S, A]]): State[S, List[A]] =
+    as.foldRight(unit[S, List[A]](Nil))(_.map2(_)(_ :: _))
 
-  def get[S]: State[S, S] = State(s => (s, s))
-
-  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
-
-  def modify[S](f: S => S): State[S, Unit] = for {
-    s <- get
-    _ <- set(f(s))
-  } yield ()
-
-  type Rand[A] = State[RNG, A]
+  /** Uses for-comprehension for illustration. */
+  def sequenceViaForComprehension[S, A](as: List[State[S, A]]): State[S, List[A]] =
+    as.foldRight(unit[S, List[A]](Nil))((a, bs) => for {aa <- a; bbs <- bs} yield aa :: bbs)
 
   /**
-    * Exercise 6.11 - Hard: To gain experience with the use of `State`, implement a finite state automaton that models a
-    * simple candy dispenser. The machine has two types of input: you can insert a coin, or you can turn the knob to
-    * dispense candy. It can be in one of two states: locked or unlocked. It also tracks how many candies are left and
-    * how many coins it contains.
+    * Lifts a state transformation to the `State` context.
     *
-    * The rules of the machine are as follows:
+    * Given a state transformation `f` of type `S => S`, this method returns a `State` instance
+    * whose `run` method, when called, will return `(Unit, S)` where the result `S` is the result of
+    * applying `f` to the input `S`.
     *
-    *   - Inserting a coin into a locked machine will cause it to unlock if there’s any candy left.
-    *   - Turning the knob on an unlocked machine will cause it to dispense candy and become locked.
-    *   - Turning the knob on a locked machine or inserting a coin into an unlocked machine does nothing.
-    *   - A machine that’s out of candy ignores all inputs.
+    * ==Derivation==
+    * val update: Machine => Machine = _.copy(locked = false)
+    * modify(update)
     *
-    * The method `simulateMachine` should operate the machine based on the list of inputs and return the number of coins
-    * and candies left in the machine at the end. For example, if the input `Machine` has 10 coins and 5 candies, and a
-    * total of 4 candies are successfully bought, the output should be `(14, 1)`.
+    * becomes:
+    * for { s <- get ; _ <- set(update(s)) } yield ()
     *
-    * Note the approach here is to map the list of inputs to transitions of the candy machine's state using the `modify`
-    * method. This method expects a function `Machine => Machine` which we create based on the rules outlined above.
-    * This yields a `List` of state transitions, which can be collapsed by using the `sequence` function we defined
-    * above.
+    * becomes:
+    * get flatMap (s => set(update(s)) map (_ => ()))
     *
-    * This attempt makes a few bad choices, though:
+    * becomes:
+    * State(s => (s, s)) flatMap (s => set(update(s)) map (_ => ()))
     *
-    *   - The machine transitions are encoded as anonymous functions; this is a little clunky
-    *   - The state of the machine (in terms of candies and coins) is produced for each input; only the last is required
-    *   - The final count of candies and coins is obtained in an unsafe way; throws `NoSuchElementException` if empty
+    * becomes:
+    * val stateUpdate: Machine => State[Machine, Unit] = s => set(update(s)) map (_ => ())
+    * State(s => { val(ss, s2) = run(s) ; stateUpdate(ss).run(s2) })
     *
-    * A better solution is provided in the second example. The major differences include:
+    * becomes:
+    * State(s => { val (s1, s2) = run(s) ; State(_ => ((), update(s1))).run(s2) })
     *
-    *   - Modeling the candy machine updates in the `Candy` object and using pattern matching for better clarity
-    *   - Cleaner implementation in `sequence`, using function composition of `update` and `modify`
-    *   - Obtaining the final machine state once, after processing all inputs, in a safe way
+    * becomes:
+    * val m = Machine(true, 0, 0)
+    * State(s => { val (s1, s2) = run(s); State(_ => ((), update(s1))).run(s2) }).run(m)
+    *
+    * becomes:
+    * ((), Machine(false, 0, 0))
+    *
     */
+  def modify[S](f: S => S): State[S, Unit] =
+    for {
+      s <- get
+      _ <- set(f(s))
+    } yield ()
 
-//  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
-//    val machineT: (Machine => Machine) => State[Machine, (Int, Int)] = f => for {
-//      _ <- modify(f)
-//      s <- get
-//    } yield (s.coins, s.candies)
-//
-//    val coinT = machineT(m => m.candies match { case 0 => m ; case _ => m.copy(locked = false, coins = m.coins + 1) })
-//    val turnT = machineT(m => m.candies match { case 0 => m ; case n => m.copy(locked = true, candies = if (m.locked) n else n - 1) })
-//
-//    // Transform inputs into a state machine
-//    val machine = sequence(inputs map { case Coin => coinT ; case Turn => turnT })
-//
-//    // We're only interested in the final result
-//    machine map (_.last)
-//  }
+  /**
+    * Returns a `State` instance whose `run` method returns the provided input.
+    *
+    * This is like an identity lifted to the `State` context.
+    *
+    * ==Derivation==
+    * val s = get[Machine]
+    *
+    * becomes:
+    * State[Machine, Machine](m => (m, m))
+    *
+    * when given a machine:
+    * s.run(Machine(true, 0, 0))
+    *
+    * becomes:
+    * (Machine(true, 0, 0), Machine(true, 0, 0))
+    */
+  def get[S]: State[S, S] = State(s => (s, s))
 
-  object Candy {
-    val update: Input => Machine => Machine = i => m => (i, m) match {
-      case (_, Machine(_, 0, _))               => m                                        // ran out of candies
-      case (Coin, Machine(false, _, _))        => m                                        // adding coin when open
-      case (Turn, Machine(true, _, _))         => m                                        // turning knob when locked
-      case (Coin, Machine(true, candy, coin))  => Machine(locked = false, candy, coin + 1) // adding coin when locked
-      case (Turn, Machine(false, candy, coin)) => Machine(locked = true, candy - 1, coin)  // turning knob when open
-    }
+  /**
+    * Returns a `State` instance whose `run` method returns the supplied state.
+    *
+    * When the `run` method is called on this `State`, its input is ignored and `S` provided
+    * to this method is returned, along with a `Unit` result.
+    *
+    * ==Derivation==
+    * val m = Machine(true, 0, 0)
+    * val s = set[Machine](m)
+    *
+    * becomes:
+    * State(_ => ((), m))
+    *
+    * when given a machine:
+    * s.run(Machine(false, 666, 666))
+    *
+    * becomes:
+    * ((), Machine(true, 0, 0))
+    */
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+}
 
-    def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = for {
+/**
+  * Exercise 6.11
+  *
+  * Hard: To gain experience with the use of `State`, implement a finite state automaton that models
+  * a simple candy dispenser. The machine has two types of input: you can insert a coin, or you can
+  * turn the knob to dispense candy. It can be in one of two states: locked or unlocked. It also
+  * tracks how many candies are left and how many coins it contains.
+  *
+  * The rules of the machine are as follows:
+  *
+  *   - Inserting a coin into a locked machine will cause it to unlock if there’s any candy left.
+  *   - Turning the knob on an unlocked machine will cause it to dispense candy and become locked.
+  *   - Turning the knob on a locked machine or inserting a coin into an unlocked machine does
+  *     nothing.
+  *   - A machine that’s out of candy ignores all inputs.
+  *
+  * The method `simulateMachine` should operate the machine based on the list of inputs and return
+  * the number of coins and candies left in the machine at the end. For example, if the input
+  * `Machine` has 10 coins and 5 candies, and a total of 4 candies are successfully bought, the
+  * output should be `(14, 1)`.
+  */
+object Candy {
+
+  sealed trait Input
+  case object Coin extends Input
+  case object Turn extends Input
+
+  // Case class representing a candy machine
+  case class Machine(locked: Boolean, candies: Int, coins: Int)
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] =
+    for {
       _ <- sequence(inputs map (update andThen modify))
       s <- get
     } yield (s.coins, s.candies)
-  }
+
+  // State transitions
+  val update: Input => Machine => Machine =
+    input => machine => (input, machine) match {
+      case (_, Machine(_, 0, _))        => machine // ignore all when no candies left
+      case (Coin, Machine(false, _, _)) => machine // ignore coin when unlocked
+      case (Turn, Machine(true, _, _))  => machine // ignore turn when locked
+      case (Coin, Machine(true, c, m))  => Machine(locked = false, c, m + 1) // valid coin
+      case (Turn, Machine(false, c, m)) => Machine(locked = true, c - 1, m) // valid turn
+    }
 }
