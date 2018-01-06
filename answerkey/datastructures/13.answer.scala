2e4f9793dfd2361641a7888e9de7f7660b4d13e9
Hélio Costa e Silva <hlegius@users.noreply.github.com>
2018-01-06 18:14:03 -0200

    Changed type ordering for `f` HOF
    in `foldRightViaFoldLeft` function

--- answerkey/datastructures/13.answer.scala
+++ answerkey/datastructures/13.answer.scala
@@ -4,7 +4,7 @@ The implementation of `foldRight` in terms of `reverse` and `foldLeft` is a comm
 The other implementations build up a chain of functions which, when called, results in the operations being performed with the correct associativity. We are calling `foldRight` with the `B` type being instantiated to `B => B`, then calling the built up function with the `z` argument. Try expanding the definitions by substituting equals for equals using a simple example, like `foldLeft(List(1,2,3), 0)(_ + _)` if this isn't clear. Note these implementations are more of theoretical interest - they aren't stack-safe and won't work for large lists.
 */
 def foldRightViaFoldLeft[A,B](l: List[A], z: B)(f: (A,B) => B): B = 
-  foldLeft(reverse(l), z)((b,a) => f(a,b))
+  foldLeft(reverse(l), z)((a,b) => f(a,b))
 
 def foldRightViaFoldLeft_1[A,B](l: List[A], z: B)(f: (A,B) => B): B = 
   foldLeft(l, (b:B) => b)((g,a) => b => g(f(a,b)))(z)
