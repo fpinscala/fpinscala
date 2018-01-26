2a77aa0dd3a470931a27a34e008840c7122c4659
Tickner, Mark <Mark.Tickner@sky.uk>
2018-01-26 17:06:50 +0000

    Add comments to exercises in chapter 3

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -50,19 +50,56 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
+  // Exercise 3.2: Implement a function for removing the first element of a `List`
   def tail[A](l: List[A]): List[A] = ???
 
+  // Exercise 3.3: Implement a function for replacing the first element of a `List` with a different value
   def setHead[A](l: List[A], h: A): List[A] = ???
 
+  // Exercise 3.4: Generalise `tail` to a function which removes the first `n` elements from a list
   def drop[A](l: List[A], n: Int): List[A] = ???
 
+  // Exercise 3.5: Implement a function which removes elements from the `List` prefix as long as they match a predicate
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
 
+  // Exercise 3.6: Implement a function that returns a `List` consisting of all but the last element of a `List`
   def init[A](l: List[A]): List[A] = ???
 
+  // Exercise 3.9: Compute the length of a list using `foldRight`
   def length[A](l: List[A]): Int = ???
 
+  // Exercise 3.10: Write a general list-recursive function that is tail-recursive
+  // `l` is the list, `z` is initial value, `f` is the combining operation
   def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
 
+  // Exercise 3.11: Write `sum`, `product`, and a function to compute the length of a list using `foldLeft`
+
+  // Exercise 3.12: Write a function that returns the reverse of a list
+
+  // Exercise 3.13: Write `foldLeft` in terms of `foldRight`
+
+  // Exercise 3.14: Implement `append` in terms of either `foldLeft` or `foldRight`
+
+  // Exercise 3.15: Write a function that concatenates a list of lists into a single list
+
+  // Exercise 3.16: Write a function that transforms a list of integers by adding 1 to each element
+
+  // Exercise 3.17: Write a function that turns each value in a `List[Double]` into a `String`
+
+  // Exercise 3.18: Write a function `map` that generalizes modifying each element in a list while maintaining the
+  // structure of the list
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
+
+  // Exercise 3.19: Write a function `filter` that removes elements from a list unless they satisfy a given predicate
+
+  // Exercise 3.20: Write a function `flatMap` that works like `map` except that the function given will return a list
+  // instead of a single result
+
+  // Exercise 3.21: Use `flatMap` to implement `filter`
+
+  // Exercise 3.22: Write a function that accepts two lists and constructs a new list by adding corresponding elements
+
+  // Exercise 3.23: Generalise the function you just wrote so that it's not specific to integers or addition
+
+  // Exercise 3.24: Implement `hasSubsequence` for checking whether a `List` contains another `List` as a subsequence
 }
