bcca5b16929fb63d17d1678559f2d1cc6222c84e
Jan Brennenstuhl <jan.brennenstuhl@zalando.de>
2018-03-08 11:18:16 +0100

    Fix Exercise 2.2 – isSorted.

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -140,7 +140,7 @@ object PolymorphicFunctions {
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = ???
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
