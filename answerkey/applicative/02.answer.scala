c131406c458cd0d7980dda81ac092e37358fb5ae
Krzysztof Palka <palkakrzysiek@gmail.com>
2018-11-29 09:17:57 +0100

    Ex 12.2 fix answer
    Apply doesn't take the second argument, it's applied to a partially
    applied function. And the moment of doing this exercise map isn't yet
    implemented, so the description is changed a bit

--- answerkey/applicative/02.answer.scala
+++ answerkey/applicative/02.answer.scala
@@ -1,11 +1,12 @@
 trait Applicative[F[_]] extends Functor[F] {
-  // `map2` is implemented by first currying `f` so we get a function
-  // of type `A => B => C`. This is a function that takes `A` and returns
-  // another function of type `B => C`. So if we map `f.curried` over an
-  // `F[A]`, we get `F[B => C]`. Passing that to `apply` along with the
-  // `F[B]` will give us the desired `F[C]`.
-  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = 
-    apply(map(fa)(f.curried), fb)
+
+  // Had we implemented `map`, `map2` could be defined by first currying `f`
+  // so we get a function of type `A => B => C`. This is a function that
+  // takes `A` and returns another function of type `B => C`. So if we map
+  // `f.curried` over an `F[A]`, we get `F[B => C]`. Passing that to `apply`
+  // along with the `F[B]` us the desired `F[C]`.
+  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    apply(map(fa)(f.curried))(fb)
 
   // We simply use `map2` to lift a function into `F` so we can apply it
   // to both `fab` and `fa`. The function being lifted here is `_(_)`,
