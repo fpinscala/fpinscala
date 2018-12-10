7e83b694d637b2ad7dabab2c48d10fb85e58d3da
Oguzhan Unlu <cengoguzhanunlu@gmail.com>
2018-12-10 16:32:23 +0300

    chapter 3: data structures - list

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,5 +1,7 @@
 package fpinscala.datastructures
 
+import scala.annotation.tailrec
+
 sealed trait List[+A] // `List` data type, parameterized on a type, `A`
 case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
 /* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
@@ -50,19 +52,111 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, t) => t
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Cons(h, Nil)
+    case Cons(_, t) => Cons(h, t)
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  @tailrec
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_,Nil) => Nil
+      case Cons(_, t) => drop(t, n-1)
+    }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  @tailrec
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, t) => if (f(h)) dropWhile(t, f) else Cons(h, t)
+  }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => sys.error("init of empty list")
+    case Cons(_, Nil) => Nil
+    case Cons(h, t) => Cons(h, init(t))
+  }
 
-  def length[A](l: List[A]): Int = ???
+  // using foldRight
+  def length[A](l: List[A]): Int = foldRight(l, 0)((_, y) => y + 1)
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  @tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+  }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
+  // using foldLeft
+  def sum3(l: List[Int]): Int = foldLeft(l, 0)(_ + _)
+
+  // using foldLeft
+  def product3(l: List[Double]): Double = foldLeft(l, 1.0)(_ * _)
+
+  // using foldLeft
+  def length2[A](l: List[A]): Int = foldLeft(l, 0)((x, _) => x + 1)
+
+  def reverse[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, t) => append(reverse(t), Cons(h, Nil))
+  }
+
+  // using foldLeft or foldRight
+  def reverse2[A](l: List[A]): List[A] = foldLeft(l, Nil: List[A])((b, a) => Cons(a, b))
+
+  // using foldRight
+  def append2[A](a1: List[A], a2: List[A]): List[A] = foldRight(a1, a2)((h, acc) => Cons(h, acc))
+
+  def concat[A](l: List[List[A]]): List[A] = foldRight(l, Nil: List[A])((h, acc) => append(h, acc))
+
+  def add1(l: List[Int]): List[Int] = foldLeft(l, Nil: List[Int])((z, h) => append(z, Cons(h+1, Nil)))
+
+  def doubleToStr(l: List[Double]): List[String] = foldRight(l, Nil: List[String])((h, acc) => append(acc, Cons(h.toString, Nil)))
+
+  def map[A,B](l: List[A])(f: A => B): List[B] = foldRight(l, Nil: List[B])((h, acc) => append(acc, Cons(f(h), Nil)))
+
+  def filter[A](as: List[A])(f: A => Boolean): List[A] =
+    foldLeft(as, Nil: List[A])((acc, h) => { if (f(h)) append(acc, Cons(h, Nil)) else acc })
+
+  // IMPORTANT NOTE about odd check at https://nrinaudo.github.io/talk-scala-best-practices/#6
+  // example to remove all odd ones, i.e. filter even ones
+  val filtered = filter(Cons(1, Cons(2, Cons(3, Nil))))(_ % 2 == 0)
+
+  def flatmap[A,B](as: List[A])(f: A => List[B]): List[B] = foldRight(as, Nil: List[B])((h, acc) => append(f(h), acc))
+
+  val flatten = flatmap(List(1,2,3))(i => List(i, i))
+
+  def filterViaFlatmap[A](as: List[A])(f: A => Boolean): List[A] = flatmap(as)(a => if (f(a)) Cons(a, Nil) else Nil )
+
+  def addCorrespondents(a1: List[Int], a2: List[Int]): List[Int] =
+    (a1, a2) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1+h2, addCorrespondents(t1, t2))
+    }
+
+  def zipWith[A](a: List[A], b: List[A])(f: (A,A) => A): List[A] =
+    (a, b) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2)(f))
+    }
+
+  def take[A](l: List[A], n: Int): List[A] = reverse(drop(reverse(l), length(l)-n))
+
+  @tailrec
+  def hasSubsequence[A](l: List[A], sub: List[A]): Boolean = {
+    val len = length(l)
+    val subLen = length(sub)
+    if (len < subLen) false
+    else if (len == subLen) if (l == sub) true else false
+    else
+     if (take(l, subLen) == sub) true
+     else hasSubsequence(tail(l), sub)
+  }
 }
