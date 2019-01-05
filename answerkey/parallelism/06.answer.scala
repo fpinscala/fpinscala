212ca59cf57a3fe8c2239045e83f6f60d8693404
Wiem Zine El Abidine <wiemzin@gmail.com>
2019-01-05 10:50:51 +0100

    improve 06.answer

--- answerkey/parallelism/06.answer.scala
+++ answerkey/parallelism/06.answer.scala
@@ -1,5 +1,2 @@
-def parFilter[A](l: List[A])(f: A => Boolean): Par[List[A]] = {
-  val pars: List[Par[List[A]]] = 
-    l map (asyncF((a: A) => if (f(a)) List(a) else List())) 
-  map(sequence(pars))(_.flatten) // convenience method on `List` for concatenating a list of lists
-}
+ def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] =
+    parMap(as.filter(f))(identity)
