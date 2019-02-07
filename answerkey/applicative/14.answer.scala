79ed1ed47b6103bbcb1c47fb3051576ed8bcacf0
sgswtky <takaya.sugisawa@gmail.com>
2019-02-07 12:42:32 +0900

    fix typo

--- answerkey/applicative/14.answer.scala
+++ answerkey/applicative/14.answer.scala
@@ -12,7 +12,7 @@ We already know this forms a `Monad`, so it's also an applicative functor:
 We can now implement `map` by calling `traverse`, picking `Id` as the `Applicative`:
 
     def map[A,B](fa: F[A])(f: A => B): F[B] =
-      traverse[Id, A, B](xs)(f)(idMonad)
+      traverse[Id, A, B](fa)(f)(idMonad)
 
 This implementation is suggestive of laws for `traverse`, since we expect this implementation to obey the usual functor laws. See the chapter notes for discussion of the laws for `Traverse`.
 
