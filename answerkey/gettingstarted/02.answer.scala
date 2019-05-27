25193fec51f12c96404b6a5a22030447fb71df84
cbwleft <cbwleft@foxmail.com>
2019-05-27 20:55:55 +0800

    Update 02.answer.scala

--- answerkey/gettingstarted/02.answer.scala
+++ answerkey/gettingstarted/02.answer.scala
@@ -2,7 +2,7 @@ def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
   @annotation.tailrec
   def go(n: Int): Boolean =
     if (n >= as.length-1) true
-    else if (gt(as(n), as(n+1))) false
+    else if (!gt(as(n), as(n+1))) false
     else go(n+1)
 
   go(0)
