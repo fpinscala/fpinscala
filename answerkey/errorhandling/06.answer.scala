fd31025c3a40d898b29d5d181757bf44998bbbb3
ajora11 <e-.-mert@hotmail.com>
2019-12-01 19:20:23 +0300

    update 06.answer.scala
    use underscores just to simplify

--- answerkey/errorhandling/06.answer.scala
+++ answerkey/errorhandling/06.answer.scala
@@ -12,7 +12,7 @@ def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
 def orElse[EE >: E, AA >: A](b: => Either[EE, AA]): Either[EE, AA] =
   this match {
     case Left(_) => b
-    case Right(a) => Right(a)
+    case _ => _
   }
 def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): 
   Either[EE, C] = for { a <- this; b1 <- b } yield f(a,b1)
