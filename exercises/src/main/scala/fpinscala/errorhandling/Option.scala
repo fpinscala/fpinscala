f30cf22c85b875fe24064f9734acc408ba473842
Bret Smith <bretrbs@icloud.com>
2020-01-18 16:23:05 -0700

    4.2 and 4.3

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -54,9 +54,19 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = ???
+  def variance(xs: Seq[Double]): Option[Double] = {
+    def internalFunc(m: Double)(x: Double): Double = math.pow(x - m, 2)
+    mean(xs).flatMap(m => mean(xs.map(internalFunc(m))))
+  }
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a, b) match {
+    case (Some(one), Some(two)) => Some(f(one, two))
+    case _ => None
+  }
+
+  def map22[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    a.flatMap(someA => b.map(someB => f(someA, someB)))
+  }
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
 
