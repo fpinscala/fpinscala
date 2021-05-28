926adc220e1457cebe89169be13a81f468d6e853
Michael Pilquist <mpilquist@gmail.com>
2021-05-28 16:53:32 -0400

    Small edits to data structures

--- answers/src/main/scala/fpinscala/datastructures/Tree.scala
+++ answers/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -35,27 +35,27 @@ object Tree:
     case Leaf(_) => 0
     case Branch(l,r) => 1 + (depth(l) max depth(r))
 
-  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match
+  def map[A,B](t: Tree[A], f: A => B): Tree[B] = t match
     case Leaf(a) => Leaf(f(a))
-    case Branch(l,r) => Branch(map(l)(f), map(r)(f))
+    case Branch(l,r) => Branch(map(l, f), map(r, f))
 
   /* 
   Like `foldRight` for lists, `fold` receives a "handler" for each of the data constructors of the type, and recursively
   accumulates some value using these handlers. As with `foldRight`, `fold(t)(Leaf(_))(Branch(_,_)) == t`, and we can use
   this function to implement just about any recursive function that would otherwise be defined by pattern matching.
   */
-  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = t match
+  def fold[A,B](t: Tree[A], f: A => B, g: (B,B) => B): B = t match
     case Leaf(a) => f(a)
-    case Branch(l,r) => g(fold(l)(f)(g), fold(r)(f)(g))
+    case Branch(l,r) => g(fold(l, f, g), fold(r, f, g))
   
   def sizeViaFold[A](t: Tree[A]): Int = 
-    fold(t)(a => 1)(1 + _ + _)
+    fold(t, a => 1, 1 + _ + _)
   
   def maximumViaFold(t: Tree[Int]): Int = 
-    fold(t)(a => a)(_ max _)
+    fold(t, a => a, _ max _)
   
   def depthViaFold[A](t: Tree[A]): Int = 
-    fold(t)(a => 0)((d1,d2) => 1 + (d1 max d2))
+    fold(t, a => 0, (d1,d2) => 1 + (d1 max d2))
   
-  def mapViaFold[A,B](t: Tree[A])(f: A => B): Tree[B] = 
-    fold(t)(a => Leaf(f(a)))(Branch(_,_))
+  def mapViaFold[A,B](t: Tree[A], f: A => B): Tree[B] = 
+    fold(t, a => Leaf(f(a)), Branch(_,_))
