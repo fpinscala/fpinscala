9baeb304bc8782c69fc219a0ab630b807311d576
Fabio <safesfabio@gmail.com>
2021-07-24 10:04:23 +0200

    Wrong callback?

--- answers/src/main/scala/fpinscala/parallelism/Nonblocking.scala
+++ answers/src/main/scala/fpinscala/parallelism/Nonblocking.scala
@@ -17,7 +17,7 @@ object Nonblocking {
     def run[A](es: ExecutorService)(p: Par[A]): A = {
       val ref = new java.util.concurrent.atomic.AtomicReference[A] // A mutable, threadsafe reference, to use for storing the result
       val latch = new CountDownLatch(1) // A latch which, when decremented, implies that `ref` has the result
-      p(es) { a => ref.set(a); latch.countDown } // Asynchronously set the result, and decrement the latch
+      p(es) { {a => ref.set(a); latch.countDown} } // Asynchronously set the result, and decrement the latch
       latch.await // Block until the `latch.countDown` is invoked asynchronously
       ref.get // Once we've passed the latch, we know `ref` has been set, and return its value
     }
