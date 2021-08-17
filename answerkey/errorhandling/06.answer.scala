414ae39508f728b725802df5f64bbd8dc2167ea8
Michael Pilquist <mpilquist@gmail.com>
2021-08-16 21:18:56 -0400

    Update chapter 5 answer key

--- answerkey/errorhandling/06.answer.scala
+++ answerkey/errorhandling/06.answer.scala
@@ -9,9 +9,9 @@ def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
     case Right(a) => f(a)
 
 def orElse[EE >: E, AA >: A](b: => Either[EE, AA]): Either[EE, AA] =
-  this match {
+  this match
     case Left(_) => b
     case Right(a) => Right(a)
-  }
+
 def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): 
   Either[EE, C] = for { a <- this; b1 <- b } yield f(a,b1)
