2234fe7735ac1b56aee8264e6bb2fa1b3c1f0bd6
Michael Pilquist <mpilquist@gmail.com>
2021-10-18 20:50:57 -0400

    More cleanup to Parsers

--- answerkey/parsing/18.answer.scala
+++ answerkey/parsing/18.answer.scala
@@ -1,19 +1,17 @@
 // We'll just give a sketch here. The basic idea is to add an additional field to `ParseError`
-
-case class ParseError(stack: List[(Location,String)] = List(),
-                      otherFailures: List[ParseError] = List()) {
+case class ParseError(stack: List[(Location,String)] = Nil,
+                      otherFailures: List[ParseError] = Nil):
 
   def addFailure(e: ParseError): ParseError =
-    this.copy(otherFailures = e :: this.otherFailures)
+    copy(otherFailures = e :: otherFailures)
   ...
-}
 
 // We then need to make sure we populate this in the implementation of `or`
-  def or[A](p: Parser[A], p2: => Parser[A]): Parser[A] =
-    s => p(s) match {
-      case Failure(e,false) => p2(s).mapError(_.addFailure(e))
+extension [A](p: Parser[A])
+  infix def or(p2: => Parser[A]): Parser[A] =
+    s => p(s) match
+      case Failure(e, false) => p2(s).mapError(_.addFailure(e))
       case r => r // committed failure or success skips running `p2`
-    }
 
 // Of course, we have to decide how to print a `ParseError` for human consumption
 // We also can expose combinators for selecting which error(s) get reported in the
