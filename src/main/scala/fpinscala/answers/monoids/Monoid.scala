092522dbcf91fb9eae3327eca0665377468fc43f
Michael Pilquist <mpilquist@gmail.com>
2022-11-13 08:06:48 -0500

    Reformat an example

--- src/main/scala/fpinscala/answers/monoids/Monoid.scala
+++ src/main/scala/fpinscala/answers/monoids/Monoid.scala
@@ -70,13 +70,15 @@ object Monoid:
   import Gen.`**`
 
   def monoidLaws[A](m: Monoid[A], gen: Gen[A]): Prop =
-    val associativity = Prop.forAll(gen ** gen ** gen):
-      case a ** b ** c =>
-        m.combine(a, m.combine(b, c)) == m.combine(m.combine(a, b), c)
-    .tag("associativity")
-    val identity = Prop.forAll(gen): a =>
-      m.combine(a, m.empty) == a && m.combine(m.empty, a) == a
-    .tag("identity")
+    val associativity = Prop
+      .forAll(gen ** gen ** gen):
+        case a ** b ** c =>
+          m.combine(a, m.combine(b, c)) == m.combine(m.combine(a, b), c)
+      .tag("associativity")
+    val identity = Prop
+      .forAll(gen): a =>
+        m.combine(a, m.empty) == a && m.combine(m.empty, a) == a
+      .tag("identity")
     associativity && identity
 
   def combineAll[A](as: List[A], m: Monoid[A]): A =
